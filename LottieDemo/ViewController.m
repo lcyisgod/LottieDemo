d7b2a21d71a6ab91a76e17d604fb6e82de836081
小龙虾 <2503978335@qq.com>
2017-05-16 09:59:19 +0800

    第一次

---
+++ LottieDemo/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  LottieDemo
+//
+//  Created by 小龙虾 on 2017/5/4.
+//  Copyright © 2017年 杭州迪火科技有限公司. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <lottie-ios/Lottie/Lottie.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    LOTAnimationView *anmiation = [LOTAnimationView animationNamed:@"fish"];
+    anmiation.frame = CGRectMake(self.view.center.x-100, self.view.center.y-100, 200, 200);
+    [self.view addSubview:anmiation];
+    [anmiation playWithCompletion:^(BOOL animationFinished) {
+        NSLog(@"123");
+    }];
+    
+    LOTAnimationView *anmiation1 = [LOTAnimationView animationNamed:@"servishero_loading"];
+    anmiation1.frame = CGRectMake(anmiation.frame.origin.x, CGRectGetMaxY(anmiation.frame)+30, 200, 200);
+    [self.view addSubview:anmiation1];
+    [anmiation1 play];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
