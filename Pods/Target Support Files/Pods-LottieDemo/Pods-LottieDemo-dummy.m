20451472ecde7e1a9cf1dff142d71430372f388b
lcyisgod <2503978335@qq.com>
2017-10-23 17:53:30 +0800

    Revert "第一次"
    This reverts commit d7b2a21d71a6ab91a76e17d604fb6e82de836081.

--- Pods/Target Support Files/Pods-LottieDemo/Pods-LottieDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LottieDemo : NSObject
-@end
-@implementation PodsDummy_Pods_LottieDemo
-@end
--- Pods/Target Support Files/Pods-LottieDemoUITests/Pods-LottieDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LottieDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LottieDemoUITests
-@end
